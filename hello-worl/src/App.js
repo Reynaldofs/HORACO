6e34e8824d1874b5436ff3f21eab04666d206e93
Reynaldofs <sales3373@gmail.com>
2023-10-17 09:35:33 -0300

    first

---
+++ hello-worl/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello World React!</h1>
+    <p>Olá para todos como está nesse dia, tão lindo de hoje, bora fazer uma coridinha !!! </p>
+    <h2>emmet configurado</h2>
+    </div>
+  );
+}
+
+export default App;
+
